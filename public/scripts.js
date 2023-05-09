75e11590c169d4e2b216c7e8417dd625d8a7223a
Tyrus <tyrus@email.unc.edu>
2023-05-09 12:27:39 -0400

    script

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,8 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function play(playerMove) {
+    const gameResult = rpsls(playerMove);
+    document.getElementById("result").innerHTML = `You chose ${gameResult.player}, the computer chose ${gameResult.opponent}. You ${gameResult.result}.`;
+}
