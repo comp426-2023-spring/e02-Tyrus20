cc72b85bb6c208f764d128d6f11ff301f550f95e
Tyrus <tyrus@email.unc.edu>
2023-05-09 13:20:12 -0400

    ignore

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,94 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-function play(playerMove) {
-    const gameResult = rpsls(playerMove);
-    document.getElementById("result").innerHTML = `You chose ${gameResult.player}, the computer chose ${gameResult.opponent}. You ${gameResult.result}.`;
+function selection() {
+    var rpsls = document.getElementById('rpsls');
+
+    var opp = document.getElementById('opp');
+
+    if (opp.checked) {
+        document.getElementById('rpsShots').style.display = "block";
+        if (rpsls.checked)
+        {
+            document.getElementById('rpslsShots').style.display = "block";
+        }
+        else {
+            document.getElementById('rpslsShots').style.display = "none";
+        }
+    }
+    else {
+        document.getElementById('rpsShots').style.display = "none";
+    }
+}
+
+// Logic for game play with requests to api
+async function play() {
+    var baseurl = window.location.href.concat('app');
+
+    if (document.getElementById('rpsls').checked) {
+        baseurl = baseurl.concat('/rpsls');
+    }
+    else {
+        baseurl = baseurl.concat('/rps');
+    }
+
+    var url = baseurl.concat('/play/');
+
+    if (!document.getElementById('rd').checked) {
+        let shot = '';
+        if (document.getElementById('rock').checked)
+            shot = 'rock';
+        if (document.getElementById('paper').checked)
+            shot = 'paper';
+        if (document.getElementById('scissors').checked)
+            shot = 'scissors';
+        if (document.getElementById('lizard').checked)
+            shot = 'lizard';
+        if (document.getElementById('spock').checked)
+            shot = 'spock';
+
+        url = url.concat(shot);
+        let response = await fetch(url);
+        let result = await response.json();
+        let output = 'You chose ' + result.player + '! ' + 'Opponent chose ' + result.opponent + '! You ' + result.result + '!';
+        document.getElementById("results").innerHTML = output;
+    }
+    else {
+        let response = await fetch(url);
+        let result = await response.json();
+        document.getElementById("results").innerHTML = result.player;
+    }
+
+    document.getElementById('play').style.display = "none";
+    document.getElementById('selection').style.display = "none";
+    document.getElementById('rules').style.display = "none";
+    document.getElementById("results").style.display = 'block';
+}
+
+// Reset page
+function reset() {
+    document.getElementById('play').style.display = "block";
+    document.getElementById('selection').style.display = "block";
+    document.getElementById('rules').style.display = "block";
+    document.getElementById("results").style.display = 'none';
+    document.getElementById('rulesText').style.display = "none";
+    
+    // Reset radios
+    document.getElementById("rps").checked = true;
+    document.getElementById("rd").checked = true;
+    document.getElementById('rpslsShots').style.display = "none";
+    document.getElementById('rpsShots').style.display = "none";
+}
+
+// Rules display
+function rules() {
+    document.getElementById('rulesText').style.display = "block";
+
+    document.getElementById('play').style.display = "none";
+    document.getElementById('selection').style.display = "none";
+    document.getElementById('rules').style.display = "none";
+    document.getElementById("results").style.display = 'none';
+    
+    // Reset radios
+
 }
--- server.js
+++ server.js
@@ -103,8 +103,6 @@ process.on('SIGINT', () => {
     })
 })
 
-//import minimist from 'minimist';
-//import express from 'express';
 import {rps} from './lib/rpsls.js';
 import {rpsls} from './lib/rpsls.js';
 
@@ -149,8 +147,4 @@ app.get('/app/rpsls/play/:shot/', (req, res) => {
 
 app.get('*', (req, res) => {
     res.status(404).send('404 NOT FOUND');
-});
-
-app.listen(port, () => {
-    console.log('Server listening on port ${port}');
 });
